c995110b3493b7b5034f95457af03d6b33545f17
GaneshTamang <tamangganesh@hotmail.com>
2022-06-26 13:46:34 +0545

    conceot clarifications

--- dartexample/collections/maps/listmaploopExample.dart
+++ dartexample/collections/maps/listmaploopExample.dart
@@ -1,23 +1,20 @@
-//iterations of list with value and each value compared to map key & if value exists adding 
-
-
+//iterations of list with value and each value compared to map key & if value exists adding
 
 void main() {
   Map<String, dynamic> menu = {'tea': 25.5, 'pizza': 250.0, 'chowmin': 140.0};
   const order = ['tea', 'ciggrette', 'pizza', 'raksi'];
   var totalprice = 0.0;
   //iteraring list of order
+  //order in loop first
   for (var customerRecievedOrder in order) {
     //comparing with map where each order cones as 'item order'
     //putting list value in map menu and accessed price value  with sending key
     //that is var currentage=person1['age']; whill will put value type in int type age in current age from map
-   
-   
-   
+
     print(customerRecievedOrder);
     //making current value from  map  that is map named menu has key   or not
-  //what is current=menu['oredereditem']; which will compare to map menu and provide null or value
-
+    //what is current=menu['oredereditem']; which will compare to map menu and provide null or value
+//checking condition existis or nill
     final currentRecievedMenuPrice = menu[customerRecievedOrder];
     //comparing with current value and doing action if meets condition
 
